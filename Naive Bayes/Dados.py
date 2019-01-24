bcb07b4fb7e28d30e7807fe278f64988f5897473
matheusm1999 <46698744+matheusm1999@users.noreply.github.com>
2019-01-24 01:10:12 -0200

    Add files via upload

---
+++ Naive Bayes/Dados.py
@@ -0,0 +1,11 @@
+import csv 
+
+def carregar_arquivo ():
+    dados = [] #dados do lado esquerdo
+    marcacoes = [] #marcacos do lado direito
+
+    arquivo = open('acesso.csv','rb')
+    leitor = csv.reader(arquivo)
+    for linha in leitor:
+        dados.append[acessou_home,acessou_como_funciona,acessou_contato]
+        marcacoes.append[comprou]
