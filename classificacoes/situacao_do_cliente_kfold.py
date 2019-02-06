fc4b0a0b06b5c367ba3d4fe793aae6fa046b3fda
matheusm1999 <46698744+matheusm1999@users.noreply.github.com>
2019-02-06 02:15:33 -0200

    Add files via upload

---
+++ classificacoes/situacao_do_cliente_kfold.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from collections import Counter
+import numpy as np
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_csv('C:/Users/mathe/OneDrive/Documentos/LABI/Python/Alura/MachineLearning/situacao_do_cliente.csv' )
+X_df = df[['recencia','frequencia', 'semanas_de_inscricao']]
+Y_df = df['situacao']
+
+Xdummies_df = pd.get_dummies(X_df)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+porcentagem_de_treino = 0.8
+
+tamanho_de_treino = int(porcentagem_de_treino * len(Y))
+# tamanho_de_validacao = len(Y) - tamanho_de_treino
+
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+
+validacao_dados = X[tamanho_de_treino:]
+validacao_marcacoes = Y[tamanho_de_treino:]
+
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes):
+    k = 10
+    scores = cross_val_score(modelo, treino_dados, treino_marcacoes, cv = k)
+    taxa_de_acerto = np.mean(scores)
+    msg = "Taxa de acerto do {}: {}".format(nome, taxa_de_acerto)
+    print(msg)
+    return taxa_de_acerto
+
+resultados = {}
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+modeloOneVsRest = OneVsRestClassifier(LinearSVC(random_state = 0))
+resultadoOneVsRest = fit_and_predict("OneVsRest", modeloOneVsRest, treino_dados, treino_marcacoes)
+resultados[resultadoOneVsRest] = modeloOneVsRest
+
+from sklearn.multiclass import OneVsOneClassifier
+modeloOneVsOne = OneVsOneClassifier(LinearSVC(random_state = 0))
+resultadoOneVsOne = fit_and_predict("OneVsOne", modeloOneVsOne, treino_dados, treino_marcacoes)
+resultados[resultadoOneVsOne] = modeloOneVsOne
+
+from sklearn.naive_bayes import MultinomialNB
+modeloMultinomial = MultinomialNB()
+resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes)
+resultados[resultadoMultinomial] = modeloMultinomial
+
+from sklearn.ensemble import AdaBoostClassifier
+modeloAdaBoost = AdaBoostClassifier()
+resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes)
+resultados[resultadoAdaBoost] = modeloAdaBoost
+
+print(resultados)
